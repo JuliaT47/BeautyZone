1b105ab13d3107f5b8b8f95ca635cc741f2982eb
Your Name <kawaiineko55@gmail.com>
2024-02-28 22:01:16 +0000

    Mobile version up to footer

---
+++ slider.js
@@ -0,0 +1,22 @@
+const swiper = new Swiper(".swiper-container", {
+  loop: true,
+  slidesPerView: 1,
+  spaceBetween: 16,
+  breakpoints: {
+    990: {
+      slidesPerView: 2,
+    },
+    1225: {
+      slidesPerView: 3,
+    },
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    bulletClass: "pagination__button",
+    bulletActiveClass: "pagination__button--active",
+  },
+  navigation: {
+    nextEl: ".slider.next",
+    prevEl: ".slider.prev",
+  },
+});
