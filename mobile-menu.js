273a518b7196f73308eaad91b7fb8d7bd621c524
Your Name <kawaiineko55@gmail.com>
2024-03-02 09:41:12 +0000

    mobile window links

--- mobile-menu.js
+++ mobile-menu.js
@@ -4,5 +4,24 @@ const menuBtnClose = document.querySelector(".menu-button-close");
 
 const toggleMenu = () => mobileMenu.classList.toggle("is-open");
 
+if (mobileMenu.classList.contains("is-open")) {
+  setTimeout(() => {
+    mobileMenu.classList.remove("is-open");
+  }, 300);
+}
+
 menuBtnOpen.addEventListener("click", toggleMenu);
 menuBtnClose.addEventListener("click", toggleMenu);
+
+const mobileMenuLinks = document.querySelectorAll(".mobile-menu__menu a");
+mobileMenuLinks.forEach((link) => {
+  link.addEventListener("click", (event) => {
+    event.preventDefault();
+    const targetHref = link.getAttribute("href");
+
+    setTimeout(() => {
+      mobileMenu.classList.remove("is-open");
+      window.location.href = targetHref;
+    }, 300);
+  });
+});
